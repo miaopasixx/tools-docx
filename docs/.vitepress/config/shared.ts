88f6668211ae47989d07a55a47c8260d59741a73
miaopasixx <a13026588708@163.com>
2025-04-11 08:56:12 +0800

    docs: 更新README.md和共享配置文件
    更新README.md文件，添加了关于Subscribe.vue和Comment.vue的说明。同时，对共享配置文件进行了注释优化和结构调整，提高了代码的可读性和维护性。

--- docs/.vitepress/config/shared.ts
+++ docs/.vitepress/config/shared.ts
@@ -1,22 +1,29 @@
 import { defineConfig, type SiteConfig } from 'vitepress'
-// 自动导入TDesign
+// 自动导入TDesign组件
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { TDesignResolver } from 'unplugin-vue-components/resolvers';
 
+// 导入工具函数
 import { createRssFileZH } from "../utils/rss";
 import { handleHeadMeta } from "../utils/handleHeadMeta";
 import { search as zhSearch } from './zh'
 
-// https://vitepress.dev/reference/site-config
+// VitePress站点配置
 export default defineConfig({
+  // 启用最后更新时间
   lastUpdated: true,
+  // 启用干净的URL
   cleanUrls: true,
+  // 忽略死链接
   ignoreDeadLinks: true,
+  // 站点地图配置
   sitemap: {
     hostname: 'https://zhiyu1998.github.io/rrorange-and-friends-weekly'
   },
+  // 头部配置
   head: [
+    // Google Analytics配置
     ["script", { async: "", src: "https://www.googletagmanager.com/gtag/js?id=G-Z6HGDC7ZBL" }],
     [
       "script",
@@ -26,7 +33,7 @@ export default defineConfig({
       gtag('js', new Date());
       gtag('config', 'G-Z6HGDC7ZBL');`,
     ],
-
+    // 网站图标配置
     [
       "link",
       {
@@ -35,32 +42,36 @@ export default defineConfig({
       },
     ],
   ],
-  // https://vitepress.dev/reference/site-config#transformhead
+  // 转换头部元数据
   async transformHead(context) {
     return handleHeadMeta(context)
   },
+  // 构建结束时生成RSS文件
   buildEnd: (config: SiteConfig) => {
     createRssFileZH(config);
   },
 
+  // 主题配置
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
+    // 网站logo配置
     logo: { src: '/favicon.png', width: 24, height: 24 },
+    // 大纲配置
     outline: [2, 4],
-
+    // 搜索配置
     search: {
       provider: "algolia",
       options: {
         ...zhSearch
       }
     },
-
+    // 显示外部链接图标
     externalLinkIcon: true,
   },
 
+  // Vite配置
   vite: {
     plugins: [
-      // ...
+      // TDesign自动导入配置
       AutoImport({
         resolvers: [TDesignResolver({
           library: 'vue-next'
